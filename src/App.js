70d812a683a9f5dcb0de056d6622cfd6f60108c9
Abhishek <abhipanchal2022@outlook.com>
2023-03-19 23:08:06 +0530

    delete patch request updated and few bugs yet to be removed

--- src/App.js
+++ src/App.js
@@ -1,87 +1,345 @@
-import './loder.scss'
+import "./loader.scss";
 import List from "./List";
 import { useState, useEffect } from "react";
 
 function App() {
+  // get request states
   let [posts, setPosts] = useState([]);
   let [postLoading, setPostLoading] = useState(false);
   let [postError, setPostError] = useState(false);
-  // error state
+  // post request states
+  let [addPostLoading, setAddPostLoading] = useState(false);
+  let [addPostError, setAddPostError] = useState(false);
+  let [addPostSuccess, setAddPostSuccess] = useState(false);
+  // Delete request states
+  let [deletePostLoading, setDeletePostLoading] = useState(false);
+  let [deletePostError, setDeletePostError] = useState(false);
+  let [deletePostSuccess, setDeletePostSuccess] = useState(false);
+
+  // Add post form state
+  let [showPostForm, setShowPostForm] = useState(false);
+
+  let [page, setPage] = useState(1);
+
+  // GET  request
   useEffect(() => {
     setPostLoading(true);
     fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => {if(res.ok){
-        return res
-      }else {
-        throw res
-      }})
+      .then((res) => {
+        if (res.ok) {
+          return res;
+        } else {
+          throw res;
+        }
+      })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setPosts(data);
         setPostLoading(false);
-      }).catch((error) => {setPostLoading(false); setPostError(true)});
+      })
+      .catch((error) => {
+        setPostLoading(false);
+        setPostError(true);
+      });
   }, []);
 
-  function postReq() {
+  // abhi to parameters de diye hai but I think REDUX is the correct way ? kya khta bhai
+  function postReq(event) {
+    event.preventDefault();
+    setAddPostLoading(true);
     fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "POST",
       body: JSON.stringify({
-        title: "Post request adding...",
-        body: "This is my first post request of all time ",
-        userId: 1,
+        title: "title here post",
+        body: "body to post ",
+        userId: 786,
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
-    }).then((response) => response.json());
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res;
+        } else {
+          throw res;
+        }
+      })
+      .then((response) => {
+        setAddPostLoading(false);
+        response.json();
+      })
+      .then((postSuccess) => {
+        setAddPostSuccess(true);
+        setShowPostForm(false);
+        window.scrollTo(0, 0);
+      })
+      .catch((error) => {
+        setAddPostLoading(false);
+        setAddPostError(true);
+        window.scrollTo(0, 0);
+      });
   }
 
+  // DELETE request
   function deletePost(postId) {
+    setDeletePostLoading(true);
     fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
       method: "DELETE",
-    });
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res;
+        } else {
+          throw res;
+        }
+      })
+      .then(() => {
+        setDeletePostLoading(false);
+        setDeletePostSuccess(true);
+        window.scrollTo(0, 0);
+      })
+      .catch(() => {
+        setDeletePostLoading(false);
+        setDeletePostError(true);
+        window.scrollTo(0, 0);
+      });
   }
 
-  let listItems = posts.map((post) => (
-    <List
-      onDelete={() => deletePost(post.id)}
-      key={post.id}
-      userid={post.userId}
-      id={post.id}
-      title={post.title}
-      body={post.body}
-    />
-  ));
+  // PATCH  request(to update information )
+  function editPost(editId) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${editId}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        title: "this is an edit request ",
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
+
+  let listItems = posts
+    .slice(page * 10 - 10, page * 10)
+    .map((post) => (
+      <List
+        onEdit={() => editPost(post.id)}
+        onDelete={() => deletePost(post.id)}
+        deleteLoading={deletePostLoading}
+        key={post.id}
+        userid={post.userId}
+        id={post.id}
+        title={post.title}
+        body={post.body}
+      />
+    ));
+    
+    const pageSelected = (pageNo) => {
+        if(pageNo >= 1 && pageNo <= posts.length /10) {
+          setPage(pageNo);
+        }
+    }
+
+  let pageNumber = [...Array(posts.length / 10)].map((_, index) => (<span className= {(page === index+1) ? "selected__page" : ""} onClick={() => pageSelected(index + 1)} key={index}>{index + 1}</span>))
 
   return (
     <div>
-      {postError}
-      {postError && <div className='alert alert-danger'>A fetch error has occured.... </div>}
-      <div className="table table-striped">
-        <tr>
-          <th>UserId</th>
-          <th>Id</th>
-          <th>Title</th>
-          <th>Body</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
+      {addPostSuccess && (
+        <div className="alert alert-success d-flex justify-content-between">
+          <p>Your POST request is Successfully executed...</p>
+          <button
+            className="btn btn-success"
+            onClick={() => setAddPostSuccess(false)}
+          >
+            Hide Alert
+          </button>
+        </div>
+      )}
+
+      {deletePostSuccess && (
+        <div className="alert alert-success d-flex justify-content-between">
+          <p>Your DELETE request is Successfully executed...</p>
+          <button
+            className="btn btn-success"
+            onClick={() => setDeletePostSuccess(false)}
+          >
+            Hide Alert
+          </button>
+        </div>
+      )}
+
+      {postError && (
+        <div className="alert alert-danger d-flex justify-content-between">
+          <p>An Error occured during GET request...</p>
+          <button
+            className="btn btn-outline-secondary"
+            onClick={() => setPostError(false)}
+          >
+            Hide Alert
+          </button>
+        </div>
+      )}
+
+      {addPostError && (
+        <div className="alert alert-danger">A POST Error has occured...</div>
+      )}
+
+      {deletePostError && (
+        <div className="alert alert-danger d-flex justify-content-between">
+          <p>Your DELETE request has been rejected...</p>
+          <button
+            className="btn btn-outline-secondary"
+            onClick={() => setDeletePostError(false)}
+          >
+            Hide Alert
+          </button>
+        </div>
+      )}
+
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>UserId</th>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
         <tbody>{listItems}</tbody>
+      </table>
+
+      <div className="pagination">
+        <span  onClick={() => pageSelected(page -1)}><b>PREV</b></span>
+        {pageNumber}
+        <span onClick={() => pageSelected(page + 1)}><b>NEXT</b></span>
       </div>
-      <button
-        className="btn btn-primary mx-auto d-flex justify-content-center m-4"
-        onClick={postReq}
-      >
-        Add post
-      </button>
 
+      {postLoading && (
+        <div className="loader">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+            alt="loader when GET method is used "
+          ></img>
+        </div>
+      )}
 
-     { postLoading  &&  <div className="loader">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"></img>
-      </div>}
+      {addPostLoading && (
+        <div className="loader ">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+            alt="loader when POST  method is used"
+          ></img>
+        </div>
+      )}
+
+      {deletePostLoading && (
+        <div className="loader ">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+            alt="loader when POST  method is used"
+          ></img>
+        </div>
+      )}
+
+      {showPostForm && <AddPostForm />}
+
+      <div className="d-flex justify-content-center mb-4">
+        {!showPostForm && (
+          <button
+            className="btn btn-primary "
+            onClick={() => setShowPostForm(true)}
+          >
+            Add New Post
+          </button>
+        )}
+      </div>
     </div>
   );
+
+  function AddPostForm() {
+    // in tino variables ko postreq function mai use kar na hai
+    let [inputTitle, setInputTitle] = useState("");
+    let [inputBody, setInputBody] = useState("");
+    let [inputUserId, setInputUserId] = useState("");
+
+    const inputTitleHandler = (event) => {
+      setInputTitle(event.target.value);
+    };
+    const inputBodyHandler = (event) => {
+      setInputBody(event.target.value);
+    };
+    const inputUserIdHandler = (event) => {
+      setInputUserId(event.target.value);
+    };
+
+    return (
+      <div className="container text-center">
+        <form className="row" onSubmit={postReq}>
+          <div className="col-4">
+            <label>
+              <b>Title :-</b>
+            </label>
+            <input
+              type="text"
+              name="title"
+              value={inputTitle}
+              onChange={inputTitleHandler}
+              placeholder="Enter POST Title here.."
+              id="title-input"
+              required
+            ></input>
+          </div>
+          <div className="col-4">
+            <label>
+              <b>Body :-</b>
+            </label>
+            <input
+              type="text"
+              name="body"
+              value={inputBody}
+              onChange={inputBodyHandler}
+              placeholder="Enter Body of POST here.."
+              required
+            ></input>
+          </div>
+          <div className="col-4">
+            <label>
+              {" "}
+              <b>UserId :-</b>
+            </label>
+            <input
+              type="text"
+              name="userid"
+              value={inputUserId}
+              onChange={inputUserIdHandler}
+              placeholder="Enter User Id here.."
+              required
+            ></input>
+          </div>
+          <div className="col m-3">
+            <button
+              className="btn btn-secondary  "
+              onClick={() => setShowPostForm(false)}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={addPostLoading}
+            >
+              {addPostLoading ? "Loading..." : "Save"}
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,17 +1,91 @@
 import React from 'react'
+import "./loader.scss"
+import "./list.scss"
 
 function List(props) {
-    
+  // let [modal, setModal] = useState(false);
+  // const modalClick = () => {
+  //   setModal(true)
+  // }
+  // console.log(props.deleteLoading)
   return (
-    <tr  >
+    <tr >
       <td >{props.userid}</td>
       <td > {props.id}</td>
-      <td >{props.title}</td>
-      <td >{props.body}</td>
-      <td><button className='btn btn-primary'>Edit</button></td>
-      <td><button className='btn btn-danger' onClick={props.onDelete}>Delete</button></td>
+      <td title={props.title}>{props.title.slice(0, 30) + "..."}</td>
+      <td title={props.body} >{props.body.slice(0, 50) + "..."}</td>
+      <td>
+        <button type='button' className='btn btn-primary' data-bs-toggle="modal" data-bs-target={"#editid" + props.id}>Edit</button>
+        <div className="modal fade" id={"editid" + String(props.id)} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Apply PATCH method on ID : {props.id}</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                Please Fill the form to update Information in Id: {props.id}
+                <form className="row" onSubmit={props.onEdit}>
+                  <div className='col'>
+                    <label><b>Title :-</b></label>
+                    <input type="text" name='title' placeholder='Enter POST Title here..' id='title-input' required ></input>
+                  </div>
+                  <div className='col'>
+                    <label><b>Body :-</b></label>
+                    <input type="text" name='body' placeholder='Enter Body of POST here..' required></input>
+                  </div>
+                  <div className='col-4'>
+                    <label> <b>UserId :-</b></label>
+                    <input type="text" name='userid' placeholder='Enter User Id here..' required></input>
+                  </div>
+                  
+                  <div className="modal-footer mt-2 col-12">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="submit" className="btn btn-info" disabled={props.deleteLoading} >{props.deleteLoading ? "Updating..." : "Update"}</button>
+              </div>
+                </form>
+              </div>
+              {/* <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" className="btn btn-info" disabled={props.deleteLoading} >{props.deleteLoading ? "Updating..." : "Update"}</button>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </td>
+
+
+      <td>
+        {/* { true && <div className="loader  ">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt='loader when POST  method is used'></img>
+      </div>} */}
+
+
+        <button type='button' className='btn btn-danger' data-bs-toggle="modal" data-bs-target={"#id" + props.id} >Delete</button>
+        <div className="modal fade" id={"id" + String(props.id)} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Apply Delete method on ID : {props.id}</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                Are you certain you want to delete row number {props.id} form your list?
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" className="btn btn-danger" disabled={props.deleteLoading} onClick={props.onDelete}>{props.deleteLoading ? "Deleting..." : "Delete"}</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </td>
     </tr>
+
   )
 }
+
+
 
 export default List
